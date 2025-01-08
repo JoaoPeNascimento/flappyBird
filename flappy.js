e6c81b8453d43e35f78597d15fd799f2ae335b0c
JoaoNascimentoPe <joao.vitorpe.1010@gmail.com>
2025-01-08 19:29:23 -0300

    Adicionando o contador de pontos

--- flappy.js
+++ flappy.js
@@ -102,13 +102,11 @@ function Passaro(alturaJogo) {
     this.setY(alturaJogo/2)
 }
 
-const barreiras = new Barreiras(700, 1200, 200, 400)
-const areaDoJogo = document.querySelector('[wm-flappy]')
+function Progresso() {
+    this.elemento = novoElemento('span', 'progresso')
+    this.atualizarPontos = pontos => {
+        this.elemento.innerHTML = pontos
+    }
+    this.atualizarPontos(0)
+}
 
-const passaro = new Passaro(700)
-areaDoJogo.appendChild(passaro.elemento)
-barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
-setInterval(() => {
-    passaro.animar()
-    barreiras.animar()
-}, 20)
